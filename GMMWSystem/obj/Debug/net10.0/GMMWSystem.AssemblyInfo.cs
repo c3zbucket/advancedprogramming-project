4e31e86eae265a916aaf840c46099a212cca33a3
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 <teirben786@gmail.com>
2026-04-07 17:55:48 +0100

    Remove redundant files

--- GMMWSystem/obj/Debug/net10.0/GMMWSystem.AssemblyInfo.cs
+++ GMMWSystem/obj/Debug/net10.0/GMMWSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GMMWSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27957c7b8164663dac7ed3977eb588a22785c0b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742da28f65c996fccc0a0174f7c338c015ed9dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GMMWSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GMMWSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
